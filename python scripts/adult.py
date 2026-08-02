3ebacbf21ac2cd67985f11de9ebf8a7dbdabeb03
kismatdahal24@gmail.com <kismatdahal24@gmail.com>
2026-08-02 12:14:54 +0545

    message

--- python scripts/adult.py
+++ python scripts/adult.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 #loading the data
-df=pd.read_csv(r'C:\Users\Dell\Desktop\data analysis\datasets\adult.csv',encoding='latin1')
+df=pd.read_csv(r'datasets\for adult\adult.csv')
 print(df.head(10))
 print(df.tail(10))
 
@@ -22,4 +22,29 @@ print(sample)
 df.isin(['?']).sum()
 
 
-#makin
+#making a piechart  of workclass
+workclass_counts = df['workclass'].value_counts()
+plt.figure(figsize=(20, 12))
+plt.pie(workclass_counts, labels=workclass_counts.index, autopct='%1.1f%%')
+plt.title('Distribution of Workclass')
+plt.savefig('images/adult visualizations/workclass_distribution.png')
+plt.show()
+
+#making native country distributin by bar graph
+import matplotlib.pyplot as plt
+
+# Native country distribution
+native_country_counts = df['native.country'].value_counts()
+
+plt.figure(figsize=(20, 12))
+plt.bar(native_country_counts.index, native_country_counts.values)
+
+plt.title('Distribution of Native Country')
+plt.xlabel('Native Country')
+plt.ylabel('Count')
+
+plt.xticks(rotation=90)      # Rotate country names
+plt.tight_layout()           # Adjust layout
+
+plt.savefig('images/adult visualizations/native_country_distribution.png')
+plt.show()
