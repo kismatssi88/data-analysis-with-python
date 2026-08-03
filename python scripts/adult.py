83ae9b48363d37f9ba42672f000ee1654245ba91
kismatdahal24@gmail.com <kismatdahal24@gmail.com>
2026-08-03 15:31:30 +0545

    cleaning data

--- python scripts/adult.py
+++ python scripts/adult.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as np
 #loading the data
-df=pd.read_csv(r'datasets\for adult\adult.csv')
+df=pd.read_csv(r'datasets\for adult\adult.csv',na_values='?')
 print(df.head(10))
 print(df.tail(10))
 
@@ -9,7 +11,7 @@ print(df.tail(10))
 print(df.shape)
 print(df.columns)
 print(df.info())
-print(df.isnull().sum())
+
 
 #descriptive satistics
 print(df.describe())
@@ -19,32 +21,30 @@ sample=df.sample(n=50)
 print(sample)
 
 #cleaning the data /removing nan vlaues
-df.isin(['?']).sum()
 
+missing=df.isin(['?']).sum()
+print(missing)
 
+#null values 
+print(df.isnull().sum())
+
+#contains so many   nan values so removing them 
+df.dropna(how='any',inplace=True)
+print(df.shape)
+
+#checking duplicate data and dropping them we get
+dup=df.duplicated().any()
+print('Are there any duplicated data?',dup)
+#removing duplicated data  
+df=df.drop_duplicates()
+print(df.shape)
 #making a piechart  of workclass
 workclass_counts = df['workclass'].value_counts()
 plt.figure(figsize=(20, 12))
 plt.pie(workclass_counts, labels=workclass_counts.index, autopct='%1.1f%%')
 plt.title('Distribution of Workclass')
 plt.savefig('images/adult visualizations/workclass_distribution.png')
+plt.tight_layout()
 plt.show()
 
-#making native country distributin by bar graph
-import matplotlib.pyplot as plt
 
-# Native country distribution
-native_country_counts = df['native.country'].value_counts()
-
-plt.figure(figsize=(20, 12))
-plt.bar(native_country_counts.index, native_country_counts.values)
-
-plt.title('Distribution of Native Country')
-plt.xlabel('Native Country')
-plt.ylabel('Count')
-
-plt.xticks(rotation=90)      # Rotate country names
-plt.tight_layout()           # Adjust layout
-
-plt.savefig('images/adult visualizations/native_country_distribution.png')
-plt.show()
